bfe4bf74dc7d23a7f5ec43e5b71b5b2f22073073
Victor Fong <vfong3@uic.edu>
2019-04-24 21:43:49 -0500

    First go at implementing code. IR Receiver and IR Sender set up for all but pins, FIXME: add pins. TODO: add LCD or other display, and perhaps a speaker or two.

--- src/main.ino
+++ src/main.ino
@@ -1,9 +1,63 @@
 #include <Arduino.h>
+#include <IRLibAll.h>
 
-void setup() {
-  // put your setup code here, to run once:
+
+/* initialize constants */
+
+const int pinTrigger;
+const int pinIRReceiver;
+const int pinIRSender;
+
+/* Declare Obects and variables! */
+
+IRrecvPCI irReceiver(pinIRReceiver);
+IRdecode decoder;
+
+IRsendNEC sender;
+
+void setup()
+{  
+  Serial.begin(9600);
+
+  delay(2000); while(!Serial);
+
+  pinMode(pinTrigger, INPUT);
+  attachInterrupt(digitalPinToInterrupt(pinTrigger), shootLaser, RISING);
+  
+  pinMode(pinIRReceiver, INPUT);
+  pinMode(pinIRSender, OUTPUT);
+
+  irReceiver.enableIRIn();
+  Serial.println("IR enabled");
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+
+void shootLaser()
+{
+  sender.send(0xfd807f);
+  irReceiver.enableIRIn();
+}
+
+
+void loop()
+{
+  if (irReceiver.getResults())
+  {
+    decoder.decode();
+    
+    if (decoder.protocolNum == NEC)
+    {
+      switch (decoder.value)
+      {
+      case 0xfd807f:  //Play/Pause:
+      {
+        //change something!
+        break;
+      }
+      
+      default:
+        break;
+      }
+    }
+  }
 }
